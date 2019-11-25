a92c81b52e09110357a3ee78cc0ea6cce2c6ffc1
adelon <22380201+adelon@users.noreply.github.com>
2019-11-26 00:17:42 +0100

    Add partial `Is` parser

--- source/Parse/Statement.hs
+++ source/Parse/Statement.hs
@@ -5,6 +5,7 @@ import Base.Parser
 import Language.Common (Var)
 import Language.Expression
 import Parse.Expression
+import Parse.Var
 
 import Data.Text (Text)
 
@@ -19,7 +20,16 @@ data Statement
   deriving (Show, Eq)
 
 statement :: Parser Statement
-statement = negated
+statement = negated <|> is
+
+is :: Parser Statement
+is = do
+  v <- math var
+  word "is"
+  adj <- adjective
+  return (v `Is` adj)
+
+adjective = undefined
 
 negated :: Parser Statement
 negated = Negated <$> do
