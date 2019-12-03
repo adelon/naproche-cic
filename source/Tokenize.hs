008806607bc15c1b252ae999adf2ea1cbba88ab5
adelon <22380201+adelon@users.noreply.github.com>
2019-12-03 23:46:04 +0100

    Complete Stream instance and add more tokenizers

--- source/Tokenize.hs
+++ source/Tokenize.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE RecordWildCards   #-}
+
 module Tokenize (Tok(..), Located(..), TokStream(..), printTok, toks) where
 
 {-
@@ -16,7 +18,11 @@ import Data.Void
 import Text.Megaparsec
 import Data.Text (Text)
 
+import Data.Foldable (asum)
+
 import qualified Data.Text as Text
+import qualified Data.List as List
+import qualified Data.List.NonEmpty as NonEmpty
 import qualified Text.Megaparsec.Char as Lex
 
 
@@ -24,6 +30,7 @@ type Tokenizer = Parsec Void Text
 
 data Tok
   = Word Text
+  | Variable Text
   | Symbol Text
   | Command Text
   | BeginEnv Text
@@ -38,6 +45,7 @@ data Delim = Invis | Paren | Brace | Bracket deriving (Show, Eq, Ord)
 printTok :: Tok -> Text
 printTok = \case
   Word w -> w
+  Variable v -> v
   Symbol s -> s
   Command cmd -> Text.cons '\\' cmd
   BeginEnv env -> "\\begin{" <> env <> "}"
@@ -103,51 +111,169 @@ instance Stream TokStream where
   takeN_ n | n <= 0 = \stream -> Just ([], stream)
   takeN_ n = \case
     TokStream _ [] -> Nothing
-    TokStream raw toks ->
-      let (consumed, toks') = splitAt n toks
+    TokStream raw ts ->
+      let (consumed, ts') = splitAt n ts
       in case nonEmpty consumed of
-        Nothing -> Just (consumed, TokStream raw toks')
-        Just toksConsumed -> Just (consumed, TokStream (Text.drop (tokensLength pxy toksConsumed) raw) toks')
-
+        Nothing -> Just (consumed, TokStream raw ts')
+        Just toksConsumed -> Just (consumed, TokStream (Text.drop (tokensLength pxy toksConsumed) raw) ts')
 
   takeWhile_ :: (Token TokStream -> Bool) -> TokStream -> (Tokens TokStream, TokStream)
-  takeWhile_ = undefined
+  takeWhile_ f (TokStream raw s) =
+    let (x, s') = List.span f s
+    in case nonEmpty x of
+      Nothing -> (x, TokStream raw s')
+      Just nex -> (x, TokStream (Text.drop (tokensLength pxy nex) raw) s')
 
   showTokens :: Proxy TokStream -> NonEmpty (Token TokStream) -> String
-  showTokens = undefined
-  -- ...
+  showTokens Proxy ts = Text.unpack $ Text.intercalate " " $ NonEmpty.toList $ printTok <$> tokenVal <$> ts
 
-toks :: Tokenizer [Tok]
-toks = some tok
+  tokensLength :: Proxy TokStream -> NonEmpty (Token TokStream) -> Int
+  tokensLength Proxy xs = sum (tokenLength <$> xs)
 
+  reachOffset :: Int -> PosState TokStream -> (String, PosState TokStream)
+  reachOffset o PosState {..} =
+    ( Text.unpack prefix <> Text.unpack restOfLine
+    , PosState
+        { pstateInput = TokStream
+            { rawInput = postStr
+            , unTokStream = post
+            }
+        , pstateOffset = max pstateOffset o
+        , pstateSourcePos = newSourcePos
+        , pstateTabWidth = pstateTabWidth
+        , pstateLinePrefix = Text.unpack prefix
+        }
+    )
+    where
+      prefix :: Text
+      prefix =
+        if sameLine
+          then Text.pack pstateLinePrefix <> preStr
+          else preStr
+      sameLine = sourceLine newSourcePos == sourceLine pstateSourcePos
+      newSourcePos =
+        case post of
+          [] -> pstateSourcePos
+          (x:_) -> startPos x
+      (pre, post) = splitAt (o - pstateOffset) (unTokStream pstateInput)
+      (preStr, postStr) = Text.splitAt tokensConsumed (rawInput pstateInput)
+      tokensConsumed =
+        case nonEmpty pre of
+          Nothing -> 0
+          Just nePre -> tokensLength pxy nePre
+      restOfLine = Text.takeWhile (/= '\n') postStr
+
+-- | Instead of adding explicit state to our tokenizer, we define mutually recursive parsers
+-- @toks@ and @mathToks@ that switch between the two tokenizing modes of normal mode and math mode.
+toks, mathToks :: Tokenizer [Tok]
+toks = do
+  t <- tok
+  case t of
+    BeginEnv "math" -> (t :) <$> mathToks
+    _ -> (t :) <$> toks
+mathToks = do
+  t <- mathTok
+  case t of
+    EndEnv "math" -> (t :) <$> toks
+    _ -> (t :) <$> mathToks
+
+-- | Parses a single normal mode token.
 tok :: Tokenizer Tok
-tok = word <|> command <|> begin <|> end <|> open <|> close
+tok = word <|> begin <|> end <|> open <|> close <|> command
 
+-- | Parses a single math mode token.
+mathTok :: Tokenizer Tok
+mathTok = var <|> begin <|> end <|> open <|> close <|> command
+
+-- | Parses a word. Words are returned casefolded, since we want to ignore their case later on.
 word :: Tokenizer Tok
-word = do
+word = lexeme do
   w <- some Lex.letterChar
-  Lex.space
-  return (Word (Text.pack w))
+  let w' = Text.toCaseFold (Text.pack w)
+  return (Word w')
 
+var :: Tokenizer Tok
+var = Variable <$> lexeme (letter <|> bb <|> greek)
+  where
+    letter :: Tokenizer Text
+    letter = Text.singleton <$> Lex.letterChar
+
+    greek :: Tokenizer Text
+    greek = do
+      Lex.char '\\'
+      l <- asum (makeSymbolParser <$> greeks)
+      return l
+
+    greeks :: [(Text,Text)]
+    greeks =
+      [ ("alpha", "α")
+      , ("beta", "β")
+      , ("gamma", "γ")
+      , ("delta", "δ")
+      -- ...
+      , ("Gamma", "Γ")
+      , ("Delta", "Δ")
+      ]
+
+    bb :: Tokenizer Text
+    bb = do
+      try (Lex.string "\\mathbb{")
+      l <- asum (makeSymbolParser <$> bbs)
+      Lex.char '}'
+      return l
+
+    bbs :: [(Text,Text)]
+    bbs =
+      [ ("A", "𝔸")
+      , ("B", "𝔹")
+      , ("C", "ℂ")
+      , ("N", "ℕ")
+      , ("P", "ℙ")
+      , ("Q", "ℚ")
+      , ("R", "ℝ")
+      , ("Z", "ℤ")
+      ]
+
+    makeSymbolParser :: (Text, b) -> Tokenizer b
+    makeSymbolParser (cmd, symb) = do
+      Lex.string cmd
+      return symb
+
+-- | Parses a TEX-style command.
 command :: Tokenizer Tok
-command = do
+command = lexeme do
   Lex.char '\\'
   cmd <- some Lex.letterChar
-  Lex.space
   return (Command (Text.pack cmd))
 
+-- | Parses the beginning of an environment. Commits only after having seen "\begin{".
 begin :: Tokenizer Tok
-begin = do
-  return (BeginEnv undefined)
+begin = lexeme do
+  try (Lex.string "\\begin{")
+  env <- some Lex.letterChar
+  Lex.char '}'
+  return (BeginEnv (Text.pack env))
 
+-- | Parses the end of an environment. Commits only after having seen "\end{".
 end :: Tokenizer Tok
-end = do
-  return (EndEnv undefined)
+end = lexeme do
+  try (Lex.string "\\end{")
+  env <- some Lex.letterChar
+  Lex.char '}'
+  return (BeginEnv (Text.pack env))
 
+-- | Parses an opening delimiter.
 open :: Tokenizer Tok
-open = do
-  return (Open undefined)
+open = paren <|> brace
+  where
+    brace = Open Brace <$ lexeme (try (Lex.string "\\{"))
+    paren = Open Paren <$ lexeme (Lex.char '(')
 
+-- | Parses a closing delimiter.
 close :: Tokenizer Tok
 close = do
   return (Close undefined)
+
+-- | Turns a tokenizer into one that consumes trailing whitespace.
+lexeme :: Tokenizer a -> Tokenizer a
+lexeme p = p <* Lex.space
