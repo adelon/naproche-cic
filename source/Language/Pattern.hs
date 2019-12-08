477aa00e192d38d62d9bd855358ad80221845633
adelon <22380201+adelon@users.noreply.github.com>
2019-12-08 22:46:23 +0100

    Make `Pattern` non-empty

--- source/Language/Pattern.hs
+++ source/Language/Pattern.hs
@@ -3,17 +3,18 @@ module Language.Pattern (Pattern(..), Shape(..)) where
 
 import Data.Text (Text, pack)
 import Data.String (IsString(..))
+import Data.List.NonEmpty as NonEmpty
 
 data Shape
   = Slot
-  | Word [Text] -- List of acceptable synonyms
+  | Word [Text]
   deriving (Show, Eq, Ord)
 
 instance IsString Shape where
   fromString w = Word [pack w]
 
-newtype Pattern = Pattern { unPattern :: [Shape]}
-  deriving (Show, Eq, Ord, Semigroup, Monoid)
+newtype Pattern = Pattern { unPattern :: NonEmpty Shape}
+  deriving (Show, Eq, Ord, Semigroup)
 
 -- Note that the Ord instance is defined so that for overlapping
 -- patterns, the one with more words is greater than the one with
--- source/Parse/Pattern.hs
+++ source/Parse/Pattern.hs
@@ -6,6 +6,7 @@ import Parse.Token (word, anyWord)
 
 import Data.Foldable (asum)
 import Data.Set (Set)
+import Data.List.NonEmpty as NonEmpty
 
 import qualified Data.Set as Set
 
@@ -18,24 +19,23 @@ patterns slot pats = asum $ try . pattern slot <$> Set.toDescList pats
 -- | Parses a pattern. Success is indicated by returning a list
 -- of the results of the parser used for slots of the pattern.
 pattern :: Parser a -> Pattern -> Parser [a]
-pattern _slot (Pattern []) = return []
-pattern slot (Pattern (s : s' : pat)) = do
+pattern slot (Pattern (s :| (s' : pat))) = do
   case s of
     Word w -> case s' of
       -- This is intended to disambiguate things like
       -- "x converges" and "x converges to y".
       Slot -> do
         try (asum (word <$> w) >> never anyWord)
-        pattern slot (Pattern (s' : pat))
+        pattern slot (Pattern (s' :| pat))
       Word _w -> do
         asum (word <$> w)
-        pattern slot (Pattern (s' : pat))
+        pattern slot (Pattern (s' :| pat))
     Slot -> do
       r <- slot
-      (r :) <$> pattern slot (Pattern (s' : pat))
-pattern slot (Pattern (s : pat)) = do
+      (r :) <$> pattern slot (Pattern (s' :| pat))
+pattern slot (Pattern (s :| [])) = do
   case s of
-    Word w -> asum (word <$> w) >> pattern slot (Pattern pat)
+    Word w -> asum (word <$> w) >> return []
     Slot -> do
       r <- slot
-      (r :) <$> pattern slot (Pattern pat)
+      return (pure r)
