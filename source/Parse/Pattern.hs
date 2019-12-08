6669b83daabbd9e9fcec686977bfeac8d63a1d73
adelon <22380201+adelon@users.noreply.github.com>
2019-12-08 22:37:22 +0100

    Add `patterns`

--- source/Parse/Pattern.hs
+++ source/Parse/Pattern.hs
@@ -5,27 +5,37 @@ import Language.Pattern (Pattern(..), Shape(..))
 import Parse.Token (word, anyWord)
 
 import Data.Foldable (asum)
+import Data.Set (Set)
+
+import qualified Data.Set as Set
+
+
+-- Precondition: list of patterns must be sorted in decreasing order.
+patterns :: Parser a -> Set Pattern -> Parser [a]
+patterns slot pats = asum $ try . pattern slot <$> Set.toDescList pats
 
 
 -- | Parses a pattern. Success is indicated by returning a list
 -- of the results of the parser used for slots of the pattern.
 pattern :: Parser a -> Pattern -> Parser [a]
-pattern _p (Pattern []) = return []
-pattern p (Pattern (s : s' : pat)) = do
+pattern _slot (Pattern []) = return []
+pattern slot (Pattern (s : s' : pat)) = do
   case s of
     Word w -> case s' of
       -- This is intended to disambiguate things like
       -- "x converges" and "x converges to y".
       Slot -> do
-        try (asum (word <$> w) >> never anyWord) >> pattern p (Pattern (s' : pat))
+        try (asum (word <$> w) >> never anyWord)
+        pattern slot (Pattern (s' : pat))
       Word _w -> do
-        asum (word <$> w) >> pattern p (Pattern (s' : pat))
+        asum (word <$> w)
+        pattern slot (Pattern (s' : pat))
     Slot -> do
-      r <- p
-      (r :) <$> pattern p (Pattern (s' : pat))
-pattern p (Pattern (s : pat)) = do
+      r <- slot
+      (r :) <$> pattern slot (Pattern (s' : pat))
+pattern slot (Pattern (s : pat)) = do
   case s of
-    Word w -> asum (word <$> w) >> pattern p (Pattern pat)
+    Word w -> asum (word <$> w) >> pattern slot (Pattern pat)
     Slot -> do
-      r <- p
-      (r :) <$> pattern p (Pattern pat)
+      r <- slot
+      (r :) <$> pattern slot (Pattern pat)
