f44ebaa091e411f75fcf5ddc62aa56847b27889c
adelon <22380201+adelon@users.noreply.github.com>
2019-12-08 22:17:27 +0100

    Add `Ord` instance to `Pattern`

--- source/Language/Pattern.hs
+++ source/Language/Pattern.hs
@@ -1,13 +1,26 @@
 module Language.Pattern (Pattern(..), Shape(..)) where
 
 
-import Data.Text (Text)
-
+import Data.Text (Text, pack)
+import Data.String (IsString(..))
 
 data Shape
   = Slot
   | Word [Text] -- List of acceptable synonyms
-  deriving (Show, Eq)
+  deriving (Show, Eq, Ord)
+
+instance IsString Shape where
+  fromString w = Word [pack w]
 
 newtype Pattern = Pattern { unPattern :: [Shape]}
-  deriving (Show, Eq, Semigroup, Monoid)
+  deriving (Show, Eq, Ord, Semigroup, Monoid)
+
+-- Note that the Ord instance is defined so that for overlapping
+-- patterns, the one with more words is greater than the one with
+-- an earlier variable slot. For example:
+--
+-- Pattern [Slot, "converges"] `compare` Pattern [Slot, "converges", "to", Slot]
+-- --> LT
+--
+-- This means we can try parsers derived from patterns in decreasing order
+-- and be sure that we get the longest match possible.
