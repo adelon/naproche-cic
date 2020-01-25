f572fd36388c7a496f0edee016d1c1a7008bd7f8
adelon <22380201+adelon@users.noreply.github.com>
2020-01-25 23:16:41 +0100

    Start sketching a Lean preamble

--- source/Language/Lean.hs
+++ source/Language/Lean.hs
@@ -1,1 +1,14 @@
 module Language.Lean where
+
+
+import qualified Data.Text as Text
+
+
+preamble :: Text
+preamble = Text.intercalate "\n\n"
+   [ "axiom omitted {p : Prop} : p"
+   -- Defines special notation for almost-universal quantification.
+   -- In the future this should be expanded to a type class for more general use.
+   , "definition almost_all_nat (p : ℕ  -> Prop) := ∃ l, ∀ n : ℕ, n ≤ l → p n"
+   , "notation `∀∞` binders `, ` r:(scoped P, almost_all_nat P) := r"
+   ]
