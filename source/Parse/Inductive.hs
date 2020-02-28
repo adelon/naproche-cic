a9a53d61ea0312d6a958c66676bd9b6742415b8e
adelon <22380201+adelon@users.noreply.github.com>
2020-02-28 20:46:16 +0100

    Start sketching inductive types

---
+++ source/Parse/Inductive.hs
@@ -0,0 +1,34 @@
+module Parse.Inductive where
+
+
+data InferRule = InferRule
+  { inferPremise :: ()
+  , inferConclusion :: ()
+  }
+
+
+
+
+-- TODO
+--
+-- We would also like to include support for mutual inductive types
+-- and patterns in inductive definitions. The following is as a first
+-- sketch of a possible phrasing for this.
+--
+--
+-- Definition. (In a "mutualinductive" environment)
+--
+-- Define n being odd and n being even as follows.
+--
+--                 n is odd       n is even
+--   ---------   -------------   ------------
+--   0 is even   n + 1 is even   n + 1 is odd
+--
+--
+--
+--
+-- One more possible phrasing:
+--
+--  *  0 is even.
+--  *  if n is odd, then n + 1 is even.
+--  *  if n is even, then n + 1 is odd.
