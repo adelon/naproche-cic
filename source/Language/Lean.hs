1c2c730e2072f89267953308d6c848510daed875
adelon <22380201+adelon@users.noreply.github.com>
2020-03-17 00:38:16 +0100

    Update Lean preamble

--- source/Language/Lean.hs
+++ source/Language/Lean.hs
@@ -7,12 +7,24 @@ import qualified Data.Text as Text
 
 
 preamble :: Text
-preamble = Text.intercalate "\n\n"
-   [ "import data.nat.basic"
+preamble = Text.intercalate "\n"
+   [ "-- BEGIN PREAMBLE"
+   , "import data.nat.basic"
    , "import data.nat.dist"
+   , ""
    , "axiom omitted {p : Prop} : p"
    -- Defines special notation for almost-universal quantification.
    -- In the future this should be expanded to a type class for more general use.
    , "definition almost_all_nat (p : ℕ  -> Prop) := ∃ l, ∀ n : ℕ, n ≤ l → p n"
    , "notation `∀∞` binders `, ` r:(scoped P, almost_all_nat P) := r"
+   , ""
+   , "mutual inductive even, odd"
+   , "with even : ℕ → Prop"
+   , "| even_zero : even 0"
+   , "| even_succ : ∀ n, odd n → even (n + 1)"
+   , "with odd : ℕ → Prop"
+   , "| odd_succ : ∀ n, even n → odd (n + 1)"
+   , ""
+   , "-- END PREAMBLE"
+   , ""
    ]
