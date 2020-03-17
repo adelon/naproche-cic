f468ea4bd41976d4a4fc56d58958469e94c5f6ed
adelon <22380201+adelon@users.noreply.github.com>
2020-03-17 20:00:08 +0100

    Start sketching structures

--- source/Parse/Structure.hs
+++ source/Parse/Structure.hs
@@ -1,1 +1,32 @@
 module Parse.Structure where
+
+
+import Base
+import Language.Expression
+import Tokenize (Tok(..))
+
+import qualified Data.Map.Strict as Map
+
+
+-- A structure can be carried by another structure (the 'Just' case),
+-- which makes it a structure extension. In this case, the new
+-- operations and properties may not overlap with those of the carrier.
+-- In the 'Nothing' case, the carrier is a set.
+--
+-- TODO:
+-- * Replace stringly type self-reference.
+
+data Struct = Struct
+   { structName :: Text
+   , structCarrier :: Maybe Struct
+   , structOps :: Map Tok Typ
+   , structProps :: [Prop]
+   }
+
+
+-- Here is a magma as a simple example of a structure.
+--
+magma :: Struct
+magma = Struct "magma" Nothing (Map.fromList [(Command "op", binOp)]) []
+  where
+     binOp = Const "self" `To` Const "self" `To` Const "self"
